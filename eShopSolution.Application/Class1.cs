aff63cb3d76388bafa0a40596a9d74eb4a5ac90d
larrylee <longlt@vnpt.vn>
2020-08-09 11:08:26 +0700

    create structure of the eshopSolution project

---
+++ eShopSolution.Application/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace eShopSolution.Application
+{
+    public class Class1
+    {
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine("WEB CSKH!!!");
+
+        }
+    }
+}
